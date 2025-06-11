144a67f6788fb9b122573b6eaf90c689697935f6
vigneshkattamudi <vkattamudi88@gmail.com>
2025-06-11 21:25:03 +0100

    Expense-shell

--- backend.sh
+++ backend.sh
@@ -65,7 +65,7 @@ unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
 
 npm install &>>$LOG_FILE
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+cp $SCRIPT_NAME/backend.service /etc/systemd/system/backend.service
 
 # load the data before running backend
 
